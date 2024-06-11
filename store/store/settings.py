8541d25ee9d8fd33b47e96fb1198d39ea3fac972
XacDacZloyKot <xacd_xacdacov@rambler.ru>
2024-06-11 19:49:59 +0300

    Connect postgres and add OAuth (github)

--- store/store/settings.py
+++ store/store/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.github',
+
     'products.apps.ProductsConfig',
     'users.apps.UsersConfig',
 ]
@@ -49,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "allauth.account.middleware.AccountMiddleware",
 ]
 
 ROOT_URLCONF = 'store.urls'
@@ -76,9 +84,13 @@ WSGI_APPLICATION = 'store.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "store_db",
+        "USER": "store_username",
+        "PASSWORD": "store_password",
+        "HOST": "localhost",
+        "PORT": "",
     }
 }
 
@@ -138,11 +150,30 @@ LOGIN_REDIRECT_URL = 'index'  # URL for login and logout (CBV)
 LOGOUT_REDIRECT_URL = 'index'  # URL for login and logout (CBV)
 
 # Sending emails
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = "smtp.yandex.ru"
-EMAIL_PORT = 465
-DEFAULT_FROM_EMAIL = 'stoor-server@yandex.ru'
-EMAIL_HOST_USER = 'stoor-server@yandex.ru'
-EMAIL_HOST_PASSWORD = 'vqrichtfitbrpkww'
-EMAIL_USE_SSL = True
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = "smtp.yandex.ru"
+# EMAIL_PORT = 465
+# DEFAULT_FROM_EMAIL = 'stoor-server@yandex.ru'
+# EMAIL_HOST_USER = 'stoor-server@yandex.ru'
+# EMAIL_HOST_PASSWORD = 'vqrichtfitbrpkww'
+# EMAIL_USE_SSL = True
 
+
+# OAuth
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+SITE_ID = 1
+
+SOCIALACCOUNT_PROVIDERS = {
+    'github': {
+        "VERIFIED_EMAIL": True,
+        'SCOPE': [
+            'user',
+        ],
+    }
+}
