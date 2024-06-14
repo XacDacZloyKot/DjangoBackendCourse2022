a2be47f3d2c1176a964e3d8a53e8cd876d97358e
XacDacZloyKot <xacd_xacdacov@rambler.ru>
2024-06-14 14:24:43 +0300

    Add debug toolbar

--- store/store/settings.py
+++ store/store/settings.py
@@ -19,66 +19,75 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&in+jk=8u$ip!9$9_@5wea(9m67rb1rawb&@oo(%cb6t6^qy&e'
+SECRET_KEY = "django-insecure-&in+jk=8u$ip!9$9_@5wea(9m67rb1rawb&@oo(%cb6t6^qy&e"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
-DOMAIN_NAME = 'http://localhost:8000'
+DOMAIN_NAME = "http://localhost:8000"
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.sites',
-    
-    'allauth',
-    'allauth.account',
-    'allauth.socialaccount',
-    'allauth.socialaccount.providers.github',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
 
-    'products.apps.ProductsConfig',
-    'users.apps.UsersConfig',
+    "django.contrib.sites",
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
+    "allauth.socialaccount.providers.github",
+
+    "products.apps.ProductsConfig",
+    "users.apps.UsersConfig",
+
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "allauth.account.middleware.AccountMiddleware",
+
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
-ROOT_URLCONF = 'store.urls'
+ROOT_URLCONF = "store.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                'products.context_processors.baskets'
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                "products.context_processors.baskets",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'store.wsgi.application'
+WSGI_APPLICATION = "store.wsgi.application"
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+    "localhost",
+]
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
@@ -99,25 +108,25 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'ru-ru'
+LANGUAGE_CODE = "ru-ru"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -128,29 +137,27 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'  # Link as a link in the browser
-STATICFILES_DIRS = (  # Link as file path
-    BASE_DIR / 'static',
-)
+STATIC_URL = "/static/"  # Link as a link in the browser
+STATICFILES_DIRS = (BASE_DIR / "static",)  # Link as file path
 
-MEDIA_URL = '/media/'  # Link as a link in the browser
-MEDIA_ROOT = BASE_DIR / 'media'  # Link as file path
+MEDIA_URL = "/media/"  # Link as a link in the browser
+MEDIA_ROOT = BASE_DIR / "media"  # Link as file path
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Users
-AUTH_USER_MODEL = 'users.User'  # Base user model
+AUTH_USER_MODEL = "users.User"  # Base user model
 
-LOGIN_URL = '/users/login/'  # URL for login(decorator 'login_required')
+LOGIN_URL = "/users/login/"  # URL for login(decorator 'login_required')
 
-LOGIN_REDIRECT_URL = 'index'  # URL for login and logout (CBV)
-LOGOUT_REDIRECT_URL = 'index'  # URL for login and logout (CBV)
+LOGIN_REDIRECT_URL = "index"  # URL for login and logout (CBV)
+LOGOUT_REDIRECT_URL = "index"  # URL for login and logout (CBV)
 
 # Sending emails
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_HOST = "smtp.yandex.ru"
 # EMAIL_PORT = 465
@@ -163,17 +170,17 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # OAuth
 
 AUTHENTICATION_BACKENDS = [
-    'django.contrib.auth.backends.ModelBackend',
-    'allauth.account.auth_backends.AuthenticationBackend',
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
 ]
 
 SITE_ID = 1
 
 SOCIALACCOUNT_PROVIDERS = {
-    'github': {
+    "github": {
         "VERIFIED_EMAIL": True,
-        'SCOPE': [
-            'user',
+        "SCOPE": [
+            "user",
         ],
     }
 }
